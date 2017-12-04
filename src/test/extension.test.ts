151ea98a8d934a8ee9d6f1adfec2bd873f75932a
Andre Weinand <weinand@mac.com>
2017-12-04 11:43:13 +0100

    cosmetics

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -14,9 +14,9 @@ import * as myExtension from '../extension';
 // Defines a Mocha test suite to group tests of similar kind together
 suite("Extension Tests", () => {
 
-    // Defines a Mocha unit test
-    test("Something 1", () => {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
-    });
+	// Defines a Mocha unit test
+	test("Something 1", () => {
+		assert.equal(-1, [1, 2, 3].indexOf(5));
+		assert.equal(-1, [1, 2, 3].indexOf(0));
+	});
 });
